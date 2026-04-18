afddfc21f2d33ffb97c9adecfae45207c0335809
Rusetska Tetiana <rusetskatata@gmail.com>
2026-04-18 13:12:27 +0200

    footer

--- src/components/fifthPage.jsx
+++ src/components/fifthPage.jsx
@@ -25,7 +25,13 @@ function FifthPage() {
         <div className="textOnP2">
             <TextOnPath />
         </div>
-
+        <div>
+            <ul className="list-none leading-[1.2em] font-[Monserrat, sans-serif] font-extralight text-[20px] flex flex-row justify-center gap-[50px] mt-[250px] mb-[15px]">
+                <li><a href="#">Impressum</a></li>
+                <li><a href="#">Datenschutz</a></li>
+                <li><a href="#">Kontakt</a></li>
+            </ul>
+        </div>
     </div>
 }
 
