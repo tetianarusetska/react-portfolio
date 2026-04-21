4a46acc9d8f42de6d15375f6dcb039f1f4b1d9ec
Rusetska Tetiana <rusetskatata@gmail.com>
2026-04-21 14:53:39 +0200

    changed router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,20 +3,6 @@ import { useEffect } from "react";
 import Home from "./pages/home.jsx";
 
 export default function Router() {
-  
-  const location = useLocation();
-
-  useEffect(() => {
-    const id = location.pathname.replace("/", "") || "home";
-    const el = document.getElementById(id);
-
-    if (el) {
-      el.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  }, [location.pathname]);
 
   return (
     <Routes>
