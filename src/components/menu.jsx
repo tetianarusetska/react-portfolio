12c13baddfc1c505edc9d7a37545e61c2fb7d045
Rusetska Tetiana <rusetskatata@gmail.com>
2026-04-22 17:12:46 +0200

    changed menu jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom"
-
 export default function Menu () {
     return <div className="menu">
         <nav>
                 <ul className="list-none flex flex-row gap-35 leading-[1.2em] mt-5 ml-[160px] font-[MelodramaLight] text-[38px]">
-                    <li><Link to="/aboutme">über mich</Link></li>
-                    <li><Link to="/projects">projekte</Link></li>
-                    <li><Link to="/contact">kontakt</Link></li>
+                    <li><a href="#aboutme">über mich</a></li>
+                    <li><a href="#projects">projekte</a></li>
+                    <li><a href="#contact">kontakt</a></li>
                 </ul>
             </nav>
     </div>
--- src/pages/fifthPage.jsx
+++ src/pages/fifthPage.jsx
@@ -1,5 +1,6 @@
 import TextOnPath from '../components/animations/textOnPath2.jsx'
 import Form from '../components/form.jsx'
+import Footer from '../components/footer.jsx'
 
 function FifthPage() {
     return <div className='h-screen w-screen '>
@@ -26,11 +27,7 @@ function FifthPage() {
             <TextOnPath />
         </div>
         <div>
-            <ul className="list-none leading-[1.2em] font-[Monserrat, sans-serif] font-extralight text-[20px] flex flex-row justify-center gap-[50px] mt-[250px] mb-[15px]">
-                <li><a href="#">Impressum</a></li>
-                <li><a href="#">Datenschutz</a></li>
-                <li><a href="#">Kontakt</a></li>
-            </ul>
+            <Footer />
         </div>
     </div>
 }
