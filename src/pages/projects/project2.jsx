9e5990a82f5bee3c62064563ccc1ec76b341d928
Rusetska Tetiana <rusetskatata@gmail.com>
2026-04-24 12:13:47 +0200

    change projects

--- src/pages/projects/project2.jsx
+++ src/pages/projects/project2.jsx
@@ -5,14 +5,17 @@ import img11 from '../../assets/images/image11.png'
 
 
 export default function Project2() {
-    return <div className="project1 flex flex-col justify-center items-center gap-[50px]">
-        <p className="text-center leading-[1.2em] font-[Montserrat, sans-serif] text-[32px] font-extralight mt-[80px]">"Portfolio" (Framer) UI, UX</p>
-        <p className="my-[10px] px-[260px] flex flex-col gap-[20px] w-[655] h-[146px] leading-[1.2em] font-[Montserrat, sans-serif] text-[20px] font-extralight mt-[80px]">
-            Ich wollte meine kreative Seite bei der Erstellung meines Portfolios zeigen. So ist diese einseitige Visitenkarte entstanden. Sie enthält eine beträchtliche Menge an Code – die gesamte Animation wurde selbst geschrieben. Außerdem plane ich, sie später mit verschiedenen Projekten zu erweitern.
-        </p>
-        <img src={img8} alt="img8" className="w-[793px] h-[489px]" />
-        <img src={img9} alt="img9" className="w-[793px] h-[489px]" />
-        <img src={img10} alt="img10" className="w-[793px] h-[489px]" />
-        <img src={img11} alt="img11" className="w-[793px] h-[489px]" />
+    return <div>
+        <a href="/" className="inline-block mt-[30px] mx-[260px] text-[25px] font-extralight">Home</a>
+        <div className="project1 flex flex-col justify-center items-center gap-[50px]">
+            <p className="text-center leading-[1.2em] font-[Montserrat, sans-serif] text-[32px] font-extralight mt-[40px]">"Portfolio" (Framer) UI, UX</p>
+            <p className="mt-[40px] px-[260px] flex flex-col gap-[20px] w-[655] h-[146px] leading-[1.2em] font-[Montserrat, sans-serif] text-[20px] font-extralight">
+                Ich wollte meine kreative Seite bei der Erstellung meines Portfolios zeigen. So ist diese einseitige Visitenkarte entstanden. Sie enthält eine beträchtliche Menge an Code – die gesamte Animation wurde selbst geschrieben. Außerdem plane ich, sie später mit verschiedenen Projekten zu erweitern.
+            </p>
+            <img src={img8} alt="img8" className="w-[793px] h-[489px]" />
+            <img src={img9} alt="img9" className="w-[793px] h-[489px]" />
+            <img src={img10} alt="img10" className="w-[793px] h-[489px]" />
+            <img src={img11} alt="img11" className="w-[793px] h-[489px]" />
+        </div>
     </div>
 }
--- src/pages/projects/project3.jsx
+++ src/pages/projects/project3.jsx
@@ -6,14 +6,17 @@ import img16 from '../../assets/images/image16.png'
 
 
 export default function Project3() {
-    return <div className="project1 flex flex-col justify-center items-center gap-[50px] mb-[20px]">
-        <p className="text-center leading-[1.2em] font-[Montserrat, sans-serif] text-[32px] font-extralight mt-[80px]">"Portfolio" (Framer) UI, UX</p>
-        <p className="my-[10px] px-[260px] flex flex-col gap-[20px] w-[655] h-[146px] leading-[1.2em] font-[Montserrat, sans-serif] text-[20px] font-extralight mt-[80px]">
-            Das Projekt wurde mit Hilfe von HTML, CSS und JavaScript umgesetzt. Das Hauptziel ist es, die Aufmerksamkeit der Kund:innen durch ein ungewöhnliches Design zu gewinnen und das Interesse am Kauf von Büchern, insbesondere bei ABC, zu wecken. Der Code kann hier eingesehen werden: Link        </p>
-        <img src={img12} alt="img12" className="w-[793px] h-[489px]" />
-        <img src={img15} alt="img15" className="w-[793px] h-[489px]" />
-        <img src={img13} alt="img13" className="w-[793px] h-[489px]" />
-        <img src={img14} alt="img14" className="w-[793px] h-[489px]" />
-        <img src={img16} alt="img16" className="w-[793px] h-[489px]" />
+    return <div>
+        <a href="/" className="inline-block mt-[30px] mx-[260px] text-[25px] font-extralight">Home</a>
+        <div className="project1 flex flex-col justify-center items-center gap-[50px] mb-[20px]">
+            <p className="text-center leading-[1.2em] font-[Montserrat, sans-serif] text-[32px] font-extralight mt-[40px]">"abc" Buchhandlung (HTML, CSS, JavaScript)</p>
+            <p className="my-[40px] px-[260px] flex flex-col gap-[20px] w-[655] h-[146px] leading-[1.2em] font-[Montserrat, sans-serif] text-[20px] font-extralight">
+                Das Projekt wurde mit Hilfe von HTML, CSS und JavaScript umgesetzt. Das Hauptziel ist es, die Aufmerksamkeit der Kund:innen durch ein ungewöhnliches Design zu gewinnen und das Interesse am Kauf von Büchern, insbesondere bei ABC, zu wecken. Der Code kann hier eingesehen werden: Link        </p>
+            <img src={img12} alt="img12" className="w-[793px] h-[489px]" />
+            <img src={img15} alt="img15" className="w-[793px] h-[489px]" />
+            <img src={img13} alt="img13" className="w-[793px] h-[489px]" />
+            <img src={img14} alt="img14" className="w-[793px] h-[489px]" />
+            <img src={img16} alt="img16" className="w-[793px] h-[489px]" />
+        </div>
     </div>
 }
